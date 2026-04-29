4b4b68dced77f08db31db38d679393670b067fc4
Elmer Menendez <elmer.menendez18@gmail.com>
2026-04-28 20:33:55 -0600

    feat: proteger rutas de api mediante middleware verificarToken

--- backend/src/routes/UsuarioRoutes.js
+++ backend/src/routes/UsuarioRoutes.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import { registrarUsuario, loginUsuario } from '../controllers/UsuarioController.js';
+import { verificarToken } from '../middlewares/auth.js';
 //creamos las rutas para los usuarios
 const router = express.Router();
 
-router.post('/registro', registrarUsuario);
 router.post('/login', loginUsuario)
 
+router.use(verificarToken);
+router.post('/registro', registrarUsuario);
 
 export default router;
