6d1979911e797bb0e4dcaaf9d2338fd5f246f4d5
Elmer Menendez <elmer.menendez18@gmail.com>
2026-05-03 18:11:56 -0600

    refactor(seguridad): implementar constantes de roles, validaciones defensivas y estandarizar JSON

---
+++ backend/src/constant/roles.js
@@ -0,0 +1,5 @@
+//exportamos los roles para usarlos en el middleware de verificación de roles y en las rutas
+export const ROLES = {
+    ADMIN: 1,   // El jefe de todo
+    AUDITOR: 2  // El que hace las auditorías
+};
--- backend/src/middlewares/verificarRol.js
+++ backend/src/middlewares/verificarRol.js
@@ -1,18 +1,23 @@
 
 export const verificarRol = (rolesPermitidos) => {
+    
+// validamos que los desarrolladores usen un arreglo para los roles permitidos 
+    if (!Array.isArray(rolesPermitidos)) {
+        throw new Error("Error: rolesPermitidos debe ser un arreglo");
+    }
+
     return (req, res, next) => {
         if (!req.usuario) {
-            return res.status(500).json({
-                exito: false,
-                mensaje: "Error interno: Se intentó verificar el rol sin validar el token."
+            return res.status(401).json({
+                exito: false, 
+                mensaje: "Acceso denegado: Usuario no autenticado correctamente."
             });
-        }  
+        }
         const rolUsuario = req.usuario.id_rol; 
-
+        // validamos que el rol del usuario esté entre los roles permitidos
         if (!rolesPermitidos.includes(rolUsuario)) {
             return res.status(403).json({
                 exito: false,
-                estado: "fallo",
                 mensaje: "Acceso denegado: No tienes los permisos necesarios para realizar esta acción."
             });
         }
